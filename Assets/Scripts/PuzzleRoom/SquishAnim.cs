a4f3143630543005476fd02e2a921c162507eb44
harjup <pikmin@gmail.com>
2014-07-26 03:28:58 -0400

    Add squishAnim for fireEnemy

---
+++ Assets/Scripts/PuzzleRoom/SquishAnim.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SquishAnim : MonoBehaviour
+{
+    void Start()
+    {
+        iTween.MoveTo(gameObject, iTween.Hash("y", transform.position.y + 3f, "time", 1f, "looptype", iTween.LoopType.pingPong, "easetype", iTween.EaseType.easeInBack));
+    }
+
+    void OnSquish()
+    {
+        
+    }
+}
